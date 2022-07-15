a35fa1a7719a88a3fee7bcf0adc1a725103afc2e
atharvatalathi <92817535+atharvatalathi@users.noreply.github.com>
2022-07-15 18:56:19 -0500

    tableau edits

--- java.js
+++ java.js
@@ -2,7 +2,7 @@ function initializeViz() {
     // JS object that points at empty div in the html
     var placeholderDiv = document.getElementById("tableauViz");
     // URL of the viz to be embedded
-    var url = "http://public.tableau.com/views/WorldIndicators/GDPpercapita";
+    var url = "https://public.tableau.com/app/profile/atharva.talathi/viz/CustomerAnalysis_16562235739850/Dashboard1";
     // An object that contains options specifying how to embed the viz
     var options = {
       width: '600px',
